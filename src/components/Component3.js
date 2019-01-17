65fbb80387b4ae403960ad8aa26b8d80e3a2eec2
Jamie Pham <jtkp66@gmail.com>
2019-01-16 23:00:27 -0800

    added Component3

---
+++ src/components/Component3.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function Component3() {
+    return (
+        <div style={c3Style}>
+            <h1>Component3</h1>
+            <p>lorem ipsum dolor, sit amet
+          wooogie boogie on the piano
+          and some other stuff.
+        </p>
+        </div>
+    )
+}
+
+const c3Style = {
+    background: 'steelblue',
+    color: 'white',
+    padding: '1.5rem 1.5rem 5rem 1.5rem'
+};
